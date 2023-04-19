1ce0dd42ffc343887615513b69b94f57c840154e
locky182 <oven8209@gmail.com>
2023-04-19 20:39:19 +0300

    Add 2 functions ArticleAppData() and ArticleApp().

--- app/src/main/java/com/locky_182/articleapp/MainActivity.kt
+++ app/src/main/java/com/locky_182/articleapp/MainActivity.kt
@@ -3,13 +3,23 @@ package com.locky_182.articleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.locky_182.articleapp.ui.theme.ArticleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ArticleApp()
+                    
                 }
             }
         }
@@ -30,14 +41,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArticleApp(){
+    ArticleAppData(image = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(id = R.string.title),
+        miniDesc = stringResource(id = R.string.miniDesc),
+        bigDesc = stringResource(id = R.string.bigDesc))
+}
+
+@Composable
+fun ArticleAppData(
+    image: Painter,
+    title: String,
+    miniDesc: String,
+    bigDesc: String,
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier){
+        Image(painter = image, contentDescription = null )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = miniDesc,
+            modifier = Modifier.padding(start = 16.dp,end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = bigDesc,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+
+        )
+        
+        
+        
+    }
+    
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ArticleAppTheme {
-        Greeting("Android")
+        ArticleApp()
+        
     }
 }
